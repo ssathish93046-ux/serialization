a4c95130a017cae7e2bbcd05cf5d5e9664b1adc1
ssathish93046-ux <ssathish93046@gmail.com>
2025-11-17 19:59:46 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IOstream1
+{
+    public class Program2
+    {
+        public static void Main()
+        {
+            System.IO.StreamWriter sw = System.IO.File.CreateText("test2.txt");
+
+            sw.Write("Innovative");
+            sw.Write(4500);
+            sw.Write(4500.555);
+
+            sw.WriteLine("hello");
+            sw.WriteLine("how are you");
+            sw.Close();
+
+            System.IO.StreamReader sr = System.IO.File.OpenText("test2.txt");
+            System.Console.WriteLine(sr.ReadToEnd());
+            sr.Close();
+        }
+
+    }
+}
